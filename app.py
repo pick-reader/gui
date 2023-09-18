53703103f2c360da747f88c1f684237c50312266
aswarthm <m.aswarthreddy@gmail.com>
2023-09-18 11:32:21 +0530

    add loading screen and option to change to woven or knitted fabric

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from pwmLed import ledBrightness
 weft_img = None
 warp_img = None
 cap_img = None
+cur_cloth_type = "Woven"
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -33,25 +34,33 @@ def add_header(r):
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	global cur_cloth_type
+
+	if(request.args.get('type')):
+		cur_cloth_type = request.args.get('type')
+
+	return render_template('index.html', cur_cloth_type=cur_cloth_type)
+
+@app.route('/menu')
+def menu():
+	return render_template('menu.html')
 
 @app.route('/vid')
 def vid():
 	return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/loading')
+def loading():
+	return render_template('loading.html')
+
 @app.route('/compute')
 def compute():
-	global weft_img, warp_img, cap_img
-	img=save_frame(OS_IMG_SAVE_PATH)
-	cap_img = img
-	ret, weft_img, warp_img = weft_warp(OS_IMG_SAVE_PATH)
-#	ret=weft_warp(img)
-	print(ret)
-	cloth = cloth_type(OS_IMG_SAVE_PATH)
-	ret = json.loads(ret)
-	##print(ret)
-#	return render_template('results.html', path=IMG_PATH, weft=ret['weft'], warp=ret['warp'], cloth=cloth)
-	return render_template('results.html', weft=ret['weft'], warp=ret['warp'], cloth=cloth)
+	global cur_cloth_type
+
+	if(cur_cloth_type.lower() == "woven"):
+		return woven()
+	elif(cur_cloth_type.lower() == "knitted"):
+		return knitted()
 
 @app.route('/led')
 def led():
@@ -87,5 +96,20 @@ def cap_img():
 	buffer = buffer.tobytes()
 	return Response(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + buffer + b'\r\n\r\n', mimetype='multipart/x-mixed-replace; boundary=frame')
 
+def woven():
+	global weft_img, warp_img, cap_img
+	img=save_frame(OS_IMG_SAVE_PATH)
+	cap_img = img
+	ret, weft_img, warp_img = weft_warp(OS_IMG_SAVE_PATH)
+#	ret=weft_warp(img)
+	print(ret)
+	cloth = cloth_type(OS_IMG_SAVE_PATH)
+	ret = json.loads(ret)
+	##print(ret)
+#	return render_template('results.html', path=IMG_PATH, weft=ret['weft'], warp=ret['warp'], cloth=cloth)
+	return render_template('woven.html', weft=ret['weft'], warp=ret['warp'], cloth=cloth)
+
+def knitted():
+	return "ok"
 
 app.run(host='0.0.0.0', port=8000)
